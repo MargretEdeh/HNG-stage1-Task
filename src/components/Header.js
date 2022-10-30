2589517bdd01b78ea5c0af4574cb7e3edce66f69
Margret Edeh <sagaedeh@gmail.com>
2022-10-30 15:00:57 -0700

    added the header

---
+++ src/components/Header.js
@@ -0,0 +1,28 @@
+import React from 'react'
+import shareAvatar from './shareAvatar.svg'
+import Links from './Links'
+
+export default function Header() {
+  return (
+    <div>
+        <main className='body'>
+  <div className='content'>
+<div className='profile-section'>
+  <img src='https://avatars.githubusercontent.com/u/109107291?v=4' className='profile-img' id='profile__img' alt='profile-img'/>
+ <div className='name-sec'>
+ <h4 id='twitter' className='name'>Edeh Margret</h4>
+ <div className='share-div'>
+<img src={shareAvatar} className='share'/>
+ </div>
+  </div> 
+  {/* <h3 id='slack' >Margret Edeh</h3> */}
+</div>
+<Links/>
+
+</div>
+
+</main>
+      
+    </div>
+  )
+}
---
+++ src/components/Links.js
@@ -0,0 +1,28 @@
+import React from 'react'
+import './links.css'
+
+export default function Links() {
+  return (
+    <div className='links-section'>
+        <div className='links' >
+            <a href='https://twitter.com/EdehMargret' id=''>Twitter Link</a>
+        </div>
+       
+        <div className='links' >
+            <a href='https://training.zuri.team/' id='btn__zuri'>Zuri Team</a>
+        </div>
+        <div className='links' >
+            <a href=' http://books.zuri.team ' id='books'>Zuri Books</a>
+        </div>
+        <div className='links' >
+            <a href='https://books.zuri.team/python-for-beginners?ref_id=<MargretEdeh> ' id='book__python'>Zuri Books</a>
+        </div>
+        <div className='links' >
+            <a href='https://background.zuri.team ' id='pitch'>Background check</a>
+        </div>
+        <div className='links' >
+            <a href='https://books.zuri.team/design-rules ' id='book__design'>Zuri design rule book</a>
+        </div>
+    </div>
+  )
+}
