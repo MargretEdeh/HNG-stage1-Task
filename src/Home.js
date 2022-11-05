c0606824f1dff6f97b9c14fbdcaa4e60cd49c70c
Margret Edeh <sagaedeh@gmail.com>
2022-11-05 13:59:42 -0700

    corrected a typo and changed btn color

--- src/Home.js
+++ src/Home.js
@@ -15,7 +15,7 @@ export default function Home() {
       <Links link='https://twitter.com/EdehMargret' id='twitter' name='Twitter Link'/>
 <Links link='https://training.zuri.team/' id='btn__zuri' name='Zuri Team' />
 <Links link=' http://books.zuri.team' id='books' name='Zuri Books'/>
-<Links link='https://books.zuri.team/python-for-beginners?ref_id=<MargretEdeh>' id='book__python' name='Phython Books' />
+<Links link='https://books.zuri.team/python-for-beginners?ref_id=<MargretEdeh>' id='book__python' name='Python Books' />
 <Links link='https://background.zuri.team' id='pitch' name='Background Check for Coders' />
 <Links link='https://books.zuri.team/design-rules' id='book__design' name='Design Book' />
 <Links link='/contact' id='contact' name='Contacts'/>
